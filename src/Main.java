29fe91a6b46cad51cd427940b5ec405591c2820c
ThanmaiG22 <bittuvsh@gmail.com>
2025-12-03 13:30:39 -0500

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Homework 1 Starter Code!");
+        
+        // TODO: Student needs to fix the logic below
+        int result = addNumbers(5, 10);
+        System.out.println("The result is: " + result);
+    }
+
+    public static int addNumbers(int a, int b) {
+        // BUG: Professor accidentally made this multiplication instead of addition
+        // You will fix this later using a Pull Request!
+        return a * b; 
+    }
+}
